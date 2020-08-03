f3a8e43ceec80385067c2794d3a60f7a2a26d9e8
JaeShinJun <jsstar1207@gmail.com>
2020-08-03 16:34:12 +0900

    Add animation to Loader components

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -10,10 +10,44 @@ const Container = styled.div`
     margin-top: 20px;
 `;
 
+const DotLoader = styled.div`
+    height: 30vw;
+    width: 30vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation-name: rotateDots;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    @keyframes rotateDots {
+        0% {
+            transform: none;
+        }
+        50% {
+            transform: rotate(0.5turn);
+        }
+        100% {
+            transform: rotate(0.5turn);
+        }
+    }
+`;
+
+const Dot = styled.span`
+    width: 10px;
+    height: 10px;
+    margin: 10px 10px;
+    background-color: #48d1cc;
+    border-radius: 50%;
+    display: inline-block;
+`;
+
 export default () => (
     <Container>
-        <span role="img" aria-label="Loading">
-            🕓
-        </span>
+        <DotLoader aria-label="Loading">
+            <Dot></Dot>
+            <Dot></Dot>
+            <Dot></Dot>
+        </DotLoader>
     </Container>
 );
